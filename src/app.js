9fcb7477360d5791bac3b14f7c8ffa84fc1a5cbf
Navaneeth Arunkumar <navaarun18@gmail.com>
2022-12-01 10:15:01 +0530

    Lab

--- src/app.js
+++ src/app.js
@@ -26,14 +26,27 @@ const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for(let i=0;i<players.length;i++){
+        // console.log(players[i])
 
+        detailedPlayers.push(
+            {
+                name:PLAYERS[i],
+                strength:getRandomStrength(),
+                image:`images/super-${i+1}.png`,
+                type:(i%2==0)?"hero":"villain"
+            }
+        )
+    }
     return detailedPlayers;
 }
+// initPlayers()
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.floor(Math.random()*100)
 }
 
 const buildPlayers = (players, type) => {
@@ -42,7 +55,16 @@ const buildPlayers = (players, type) => {
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
-
+        for(let i=0;i<players.length;i++){
+            if(players[i].type==type) {
+                fragment+=`
+                    <div class="player">
+                        <img src="${players[i].image}" alt="">
+                        <div class="name">${players[i].name}</div>
+                        <div class="strength">${players[i].strength}</div>
+                    </div> `
+                }
+            }
     return fragment;
 }
 // Display players in HTML
